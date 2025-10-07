3bbc02649d2a5e37772f747b58c3480efeb7ccf0
kalllak17 <kalllak17@gmail.com>
2025-10-07 13:00:34 -0400

    fixed path to home.html

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var path = require('path');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, 'public', 'homepage.html'));
+  res.sendFile(path.join(__dirname, '..', 'public', 'home.html'));
 });
 
 module.exports = router;
