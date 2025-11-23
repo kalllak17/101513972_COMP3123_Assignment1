deedd8aef87f395d4737a63607ca8f4266eebb18
kalllak17 <kalllak17@gmail.com>
2025-11-23 15:34:57 -0500

    commented multer

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 require('dotenv').config();
-const multer = require('multer')
+// const multer = require('multer')
 
 const PORT = process.env.PORT || 5000;
 
@@ -35,8 +35,8 @@ app.use(cors({
     allowedHeaders: ['Content-Type','Authorization']
 }));
 
-const storage = multer.memoryStorage();
-const upload = multer({ storage: storage });
+// const storage = multer.memoryStorage();
+// const upload = multer({ storage: storage });
 
 initializePassport(passport);
 app.use(passport.initialize());
